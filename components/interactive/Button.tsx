6aaba357c633acc3f73a29d82a1396f6aed10123
Kai <orozobekov.kai@gmail.com>
2024-08-18 23:41:08 -0400

    style: prettier integration

--- components/interactive/Button.tsx
+++ components/interactive/Button.tsx
@@ -1,61 +1,128 @@
-import { ComponentProps, ReactElement } from "react"
-import { twMerge } from 'tailwind-merge'
+import { ComponentProps, ReactElement } from "react";
+import { twMerge } from "tailwind-merge";
 import { VariantProps, cva } from "class-variance-authority";
 import Link from "next/link";
 import LoadingSpinner from "../icons/LoadingSpinner";
 
-
-const buttonVariants = cva(["rounded-lg", "py-2", "px-4", "disabled:bg-gray-200", "disabled:text-slate-500/50", "disabled:font-medium", "disabled:cursor-not-allowed",
-    "cursor-pointer", "transition-colors", "duration-300", "inline-flex", "justify-center", 'items-center', "gap-x-2", "focus-visible:outline-none",], {
-    variants: {
-        intent: {
-            primary: [
-                "bg-primary",
-                "hover:bg-primary-dark",
-                "active:bg-primary-light",
-                "text-white"
-            ],
-            danger: [
-                "bg-danger",
-                "hover:bg-danger-dark",
-                "active:bg-danger-light",
-                "text-white"
-            ],
+const buttonVariants = cva(
+    [
+        "rounded-lg",
+        "py-2",
+        "px-4",
+        "disabled:bg-gray-200",
+        "disabled:text-slate-500/50",
+        "disabled:font-medium",
+        "disabled:cursor-not-allowed",
+        "cursor-pointer",
+        "transition-colors",
+        "duration-300",
+        "inline-flex",
+        "justify-center",
+        "items-center",
+        "gap-x-2",
+        "focus-visible:outline-none",
+    ],
+    {
+        variants: {
+            intent: {
+                primary: [
+                    "bg-primary",
+                    "hover:bg-primary-dark",
+                    "active:bg-primary-light",
+                    "text-white",
+                ],
+                danger: [
+                    "bg-danger",
+                    "hover:bg-danger-dark",
+                    "active:bg-danger-light",
+                    "text-white",
+                ],
+            },
+            size: {
+                sm: "text-sm",
+                md: "text-base",
+                lg: "text-lg",
+            },
         },
-        size: {
-            sm: "text-sm",
-            md: "text-base",
-            lg: "text-lg"
-        }
+        defaultVariants: {
+            intent: "primary",
+            size: "md",
+        },
     },
-    defaultVariants: {
-        intent: "primary",
-        size: "md"
-    }
-})
+);
 
-type ButtonVariants = VariantProps<typeof buttonVariants>
+type ButtonVariants = VariantProps<typeof buttonVariants>;
 
-type ButtonProps = ComponentProps<'button'> & ButtonVariants & {
-    label?: string,
-    as?: "button" | "link",
-    fullWidth?: boolean,
-    icon?: ReactElement,
-    iconAlignment?: "left" | "right",
-    target?: ComponentProps<'a'>['target'];
-    href?: ComponentProps<'a'>['href'];
-    loading?: boolean;
-}
+type ButtonProps = ComponentProps<"button"> &
+    ButtonVariants & {
+        label?: string;
+        as?: "button" | "link";
+        fullWidth?: boolean;
+        icon?: ReactElement;
+        iconAlignment?: "left" | "right";
+        target?: ComponentProps<"a">["target"];
+        href?: ComponentProps<"a">["href"];
+        loading?: boolean;
+    };
 
-const Button = ({ label, className, size = "md", intent = "primary", as = "button", fullWidth = false, href, target, icon, iconAlignment = "right", loading, disabled, ...props }: ButtonProps) => {
+const Button = ({
+    label,
+    className,
+    size = "md",
+    intent = "primary",
+    as = "button",
+    fullWidth = false,
+    href,
+    target,
+    icon,
+    iconAlignment = "right",
+    loading,
+    disabled,
+    ...props
+}: ButtonProps) => {
     if (as === "link")
-        return <Link href={href ?? ""} target={target} className={twMerge(buttonVariants({ intent, size }), `${!label && icon && "p-2"}`, `${fullWidth && "w-full"}`, className)}>
-            <>{iconAlignment === "left" && icon}{label}{iconAlignment === "right" && icon}</>
-        </Link>
+        return (
+            <Link
+                href={href ?? ""}
+                target={target}
+                className={twMerge(
+                    buttonVariants({ intent, size }),
+                    `${!label && icon && "p-2"}`,
+                    `${fullWidth && "w-full"}`,
+                    className,
+                )}
+            >
+                <>
+                    {iconAlignment === "left" && icon}
+                    {label}
+                    {iconAlignment === "right" && icon}
+                </>
+            </Link>
+        );
 
-    return <button disabled={disabled} aria-disabled={disabled} {...props} className={twMerge(buttonVariants({ intent, size }), `${fullWidth && "w-full"}`, `${!label && icon && "p-2"}`, className)}>
-        {!loading ? <>{iconAlignment === "left" && icon}{label}{iconAlignment === "right" && icon}</> : <LoadingSpinner />}
-    </button>
-}
+    return (
+        <button
+            disabled={disabled}
+            aria-disabled={disabled}
+            {...props}
+            className={twMerge(
+                buttonVariants({ intent, size }),
+                `${fullWidth && "w-full"}`,
+                `${!label && icon && "p-2"}`,
+                className,
+            )}
+        >
+            {!loading ? (
+                <>
+                    {iconAlignment === "left" && icon}
+                    {label}
+                    {iconAlignment === "right" && icon}
+                </>
+            ) : (
+                <LoadingSpinner />
+            )}
+        </button>
+    );
+};
 
-export default Button
+export default Button;
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -4,90 +4,89 @@ import { FaPlus } from "react-icons/fa6";
 import { IoIosCheckmark } from "react-icons/io";
 
 const meta = {
-    title: "Button",
-    component: Button,
-    args: {
-        label: "Button Text",
-        size: "md",
-        intent: "primary",
-        fullWidth: false,
+  title: "Button",
+  component: Button,
+  args: {
+    label: "Button Text",
+    size: "md",
+    intent: "primary",
+    fullWidth: false,
+  },
+  argTypes: {
+    target: {
+      control: "select",
+      options: ["_self", "_blank", "_parent", "_top", "_unfencedTop"],
+      table: { disable: true },
     },
-    argTypes: {
-        target: {
-            control: "select",
-            options: ["_self", "_blank", "_parent", "_top", "_unfencedTop"],
-            table: { disable: true },
-        },
-        href: {
-            control: "text",
-            table: { disable: true },
-        },
-        intent: {
-            control: 'select',
-            options: ["primary", "danger"]
-        },
-        size: {
-            control: 'select',
-            options: ['sm', "md", "lg"]
-        },
-        as: {
-            table: { disable: true },
-        },
-        icon: {
-            control: "select",
-            options: ["Plus", "Checkmark"],
-            mapping: {
-                Plus: <FaPlus />,
-                Checkmark: <IoIosCheckmark size={20} />
-            }
-        }
+    href: {
+      control: "text",
+      table: { disable: true },
     },
-    tags: ["autodocs"],
+    intent: {
+      control: "select",
+      options: ["primary", "danger"],
+    },
+    size: {
+      control: "select",
+      options: ["sm", "md", "lg"],
+    },
+    as: {
+      table: { disable: true },
+    },
+    icon: {
+      control: "select",
+      options: ["Plus", "Checkmark"],
+      mapping: {
+        Plus: <FaPlus />,
+        Checkmark: <IoIosCheckmark size={20} />,
+      },
+    },
+  },
+  tags: ["autodocs"],
 } satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const PrimaryButton: Story = {
-    argTypes: {
-        intent: {
-            table: { disable: true },
-        },
+  argTypes: {
+    intent: {
+      table: { disable: true },
     },
+  },
 };
 
 export const DangerButton: Story = {
-    args: {
-        intent: "danger",
+  args: {
+    intent: "danger",
+  },
+  argTypes: {
+    intent: {
+      table: { disable: true },
     },
-    argTypes: {
-        intent: {
-            table: { disable: true },
-        },
-    },
+  },
 };
 
 export const DisabledButton: Story = {
-    args: {
-        disabled: true,
-    },
+  args: {
+    disabled: true,
+  },
 };
 
 export const ButtonWithIcon: Story = {
-    args: {
-        icon: <FaPlus />
-    }
+  args: {
+    icon: <FaPlus />,
+  },
 };
 
-
 export const OnlyIconButton: Story = {
-    args: {
-        label: "",
-        icon: <FaPlus />
+  args: {
+    label: "",
+    icon: <FaPlus />,
+  },
+  argTypes: {
+    label: {
+      table: { disable: true },
     },
-    argTypes: {
-        label: {
-            table: { disable: true },
-        },
-    },
+  },
 };
